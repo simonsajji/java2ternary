c6aab3a1dc2c818c289429b0daffe721bab93842
simonsajji <85159792+simonsajji@users.noreply.github.com>
2021-09-12 17:47:52 +0530

    Add files via upload

---
+++ com/simon/ternary.java
@@ -0,0 +1,19 @@
+package com.simon;
+
+public class ternary {
+	
+	public static void main(String[] args) {
+		int x=40;
+		int y=60;
+		int z=35;
+		int Max;
+		String kin= "hello there ";
+		
+		
+		kin= (x>y) ? (x>z ? "x is the greatest ":"z is the greatest") : (y>z ? "y is the greatest":"z is the greatest");
+		System.out.println(kin);
+		
+				
+	}
+
+}
